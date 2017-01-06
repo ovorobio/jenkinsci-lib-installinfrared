b5a74192ab131d082f953f7edc0dc06558a9c81d
Oleksandr Vorobiov <ovorobio@redhat.com>
2017-01-06 13:32:45 +0200

    First commit to the library

---
+++ src/com/redhat/jenkinsci/installinfrared/IRInstall.groovy
@@ -0,0 +1,20 @@
+package com.redhat.jenkinsci.install.installinfrared;
+
+def InstallInfraRed() {
+    git url: "https://github.com/rhosqeauto/InfraRed.git"
+    git url: "https://code.engineering.redhat.com/gerrit/rhos-infrared"
+    sh """
+        virtualenv $venv
+        . $venv/bin/activate
+        pip install -U pip
+        pip install $WORKSPACE/InfraRed/.
+        host=$HOSTNAME
+
+        pushd InfraRed
+        pip install . > $WORKSPACE/pip.log
+        pip list > $WORKSPACE/pip_packages.txt
+        cp ansible.cfg.example ansible.cfg
+        mkdir -p "$venv/cp"
+        sed -i "s|^control_path.*|control_path = $venv/cp/%%h-%%r|" ansible.cfg
+    """
+}
